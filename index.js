d45da391cdb17ee981aef2013801aa0267518d2e
Oliverio <oliverio.junior2@gmail.com>
2022-03-23 06:00:48 -0300

    Escrever função usando Promise

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+import { throws } from 'assert';
 import chalk from 'chalk';
+import fs from 'fs';
 
-console.log(chalk.blue('vamos') +   'aqui')
+
+function patternErr(err){
+    throw new Error(chalk.red(err.code));
+}
+/* function takeFile(wayFile){
+    const encoding = 'utf-8'
+    fs.readFile(wayFile, encoding, (err, text)=>{
+        if(err){
+            patternErr(err);
+        }
+           console.log(chalk.green(text))
+    })
+} */
+function takeFile(wayFile){
+    const encoding = 'utf-8';
+    fs.promises
+    .readFile(wayFile, encoding)
+    .then((text)=> chalk.green(console.log(text)))
+    . catch((err)=> patternErr(err)); 
+}
+
+takeFile('./text.md')
+
---
+++ text.md
@@ -0,0 +1,5 @@
+A Matemática, às vezes, pode parecer difícil, complicada, até mesmo fora da realidade. Mas também pode ser instigante e bela. Para nós que gostamos da Matemática, nem pensamos nisso, pois já faz parte de nossas vidas.
+
+Uma Identidade Matemática é uma igualdade que permanece verdadeira para quaisquer que sejam os valores das variáveis que nela apareçam, diferentemente de uma Igualdade Matemática que pode ser verdadeira somente sob condições particulares.
+
+A Identidade de Euler reúne, talvez, os cinco números mais importantes da Matemática 0, 1, i, e, π em uma simples igualdade: ei π+1=0.
